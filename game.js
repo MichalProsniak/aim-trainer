88409e89673128ac12b4b17354160b9defa2f1c8
Damian Tarczoń <tarczon96@gmail.com>
2022-03-14 18:24:17 +0100

    Board and target moving done

--- game.js
+++ game.js
@@ -5,3 +5,9 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+
+function changePosition() {
+    let btn = document.getElementById('btn');
+    btn.style.top = Math.floor((Math.random() * 750) + 1) + "px";
+    btn.style.left = Math.floor((Math.random() * 900) + 1) + "px";
+}
