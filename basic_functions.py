60e53c6410641a47c0f3cec6ee2dbd9fa7816c9c
Mateusz Skiba <mateusz.skiba858@gmail.com>
2022-03-17 11:54:07 +0100

    logout

--- basic_functions.py
+++ basic_functions.py
@@ -8,3 +8,4 @@ def get_hashed_password(plain_text_password):
 def verify_password(typed_password, password_in_database):
     hashed_bytes_password = password_in_database.encode('utf-8')
     return bcrypt.checkpw(typed_password.encode('utf-8'), hashed_bytes_password)
+
--- server.py
+++ server.py
@@ -17,13 +17,17 @@ def main():
 def login():
     if request.method == "GET":
         return render_template('login.html')
+    if request.method == "GET" and "user" in session:
+        flash("You are logged in", "info")
+        return redirect('/')
     username = request.form['username']
     password = request.form['password']
 
     if data_base_functions.username_check(username):
-        hashed_password = data_base_functions.get_user_password(username)
-        if basic_functions.verify_password(password, hashed_password):
+        # hashed_password = data_base_functions.get_user_password(username)
+        if data_base_functions.get_user_password(username) == password:
             session['user'] = username
+            flash(f"You have been logged in {username}!", "info")
             return redirect('/')
         else:
             error = "Incorrect username or password"
@@ -32,20 +36,29 @@ def login():
         error = "Incorrect username or password"
         return render_template('login.html', error=error)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
     user_name = request.form['username']
-    user_password = request.form['password'].encode("utf-8")
+    user_password = request.form['password']
     if not data_base_functions.username_check(user_name):
-        user_password = basic_functions.get_hashed_password(user_password)
+        # user_password = basic_functions.get_hashed_password(user_password)
         data_base_functions.registration_to_db(user_name, user_password)
     else:
         error = "User name already in use. Try again."
         return render_template('register.html', error=error)
     return redirect(url_for('main'))
 
+
+@app.route('/logout')
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out {user}!", "info")
+    session.pop("user", None)
+    return redirect(url_for("main"))
 
 
 if __name__ == '__main__':
